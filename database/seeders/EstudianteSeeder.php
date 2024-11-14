b74ec6cee9abb9554c3920790cdd49df7acf476c
Jorge Omner Arias Olea~ <omnerarias001@gmail.com>
2024-11-14 13:51:53 -0600

    antes de correcion a los pdf´s

--- database/seeders/EstudianteSeeder.php
+++ database/seeders/EstudianteSeeder.php
@@ -37,6 +37,8 @@ class EstudianteSeeder extends Seeder
         $estudiante->direccion = "4ta norte y 2da poniente Edif 4 A";
         $estudiante->institucion_seguridad_social = "IMSS";
         $estudiante->numero_de_seguridad_social = "2001";
+        $estudiante->proyecto_id=2;
+        $estudiante->carrera_id = 1;
         $estudiante->save(); 
     }
 
